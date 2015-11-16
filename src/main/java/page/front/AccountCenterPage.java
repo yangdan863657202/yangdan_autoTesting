d9e2ec2e10abeac774988d0745a5ddfb339ab330
yangdan863657202@163.com <yangdan863657202@163.com>
2015-11-16 17:31:20 +0800

    11

--- src/main/java/page/front/AccountCenterPage.java
+++ src/main/java/page/front/AccountCenterPage.java
@@ -3,9 +3,12 @@ package page.front;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import util.WebDriverUtil;
 import xpath.front.AccountCenterPageXpath;
+
+import java.util.List;
 
 /**
  * Created by yangdan
@@ -19,4 +22,19 @@ public class AccountCenterPage {
         String jsStr = "document.getElementByXpath('/html/body/div[3]/div[1]/div/ul/li[3]/div/a[1]').click();";
         js.executeScript(jsStr);
     }
+
+    public void getFundDetail(){
+        List<WebElement> trs = accountInfoPage.getPanelDataGridHTableTrs(driver, "账户还款计划信息");
+        for (WebElement tr : trs) {
+            List<WebElement> tds= tr.findElements(By.xpath("./td"));
+            for (WebElement td : tds) {
+                String attribute=	td.getAttribute("field");
+                System.out.println(attribute+ "  "+td.findElement(By.tagName("div")).getText());
+            }
+            System.out.println("--------------");
+        }
+
+    }
+
+
 }
