fcaf8960a9245ac4822a4aaf6ca4bbecada3233f
yangdan <yangdan863657202@163.com>
2015-12-14 17:19:49 +0800

    11

---
+++ src/test/java/pccbInterface/GetAuthenticationTest.java
@@ -0,0 +1,62 @@
+package pccbInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import util.Des;
+import util.Md5;
+
+/**
+ * Created by yangdan
+ */
+public class GetAuthenticationTest {
+    @Test
+    public static void getAuthenticationTest() throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost post = new HttpPost("http://api.pccb.com/apitest/get_authentication/");
+            List<NameValuePair> list = new ArrayList<NameValuePair>();
+            list.add(new BasicNameValuePair("app_type","1"));
+            list.add(new BasicNameValuePair("userid", Des.encryptDES("82044")));
+            list.add(new BasicNameValuePair("sign_key", Md5.md5( "82044"+"appGetAuthentication" )));
+            System.out.println("-----" + Md5.md5("82044" + "pccbUserCheckIn") + "-----");
+            System.out.println("-----" + Des.encryptDES("82044") + "-----");
+            post.setEntity(new UrlEncodedFormEntity(list));
+            CloseableHttpResponse response = httpclient.execute(post);
+
+            try {
+                System.out.println(response.getStatusLine());
+                HttpEntity entity2 = response.getEntity();
+                String entity = EntityUtils.toString(entity2);
+                if(entity.contains("")){
+                    System.out.println("Success!");
+                    System.out.println("response content:" + entity);
+                }
+                else
+                {
+                    System.out.println("Failure!");
+                    System.out.println("response content:" + entity);
+                    AssertJUnit.fail(entity);
+                }
+            } finally {
+                response.close();
+            }
+        }
+        finally {
+            httpclient.close();
+
+        }
+
+    }
+}
