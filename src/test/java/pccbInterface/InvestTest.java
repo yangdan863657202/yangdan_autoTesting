36df9e9bceb8b7b9fdca0cecc58b3f818a1a59a7
yangdan <yangdan863657202@163.com>
2015-12-18 14:55:55 +0800

    11

--- src/test/java/pccbInterface/InvestTest.java
+++ src/test/java/pccbInterface/InvestTest.java
@@ -13,39 +13,55 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import page.front.HomePage;
+import service.front.GetTokenService;
+import service.front.ReturnUrlService;
 import util.Des;
 import util.Md5;
+import util.WebDriverUtil;
 
 /**
  * Created by yangdan
  */
 public class InvestTest {
+    private WebDriver driver;
+    HomePage homePage=new HomePage();
+
+    @BeforeTest
+    public void setUp() {
+        //driver = WebDriverUtil.getWebDriver(BrowserType.CHROME);
+        driver=new ChromeDriver();
+    }
     @Test
-    public static void investTest() throws Exception {
+    public  void investTest() throws Exception {
+        driver.get(ReturnUrlService.returnUrlService());
+        driver.findElement(By.xpath("//*[@id=\"payPassword\"]")).sendKeys("123abc");
+        driver.findElement(By.xpath("//*[@id=\"btnSubmit\"]")).click();
+        WebDriverUtil.sleep(2000);
+        String s=driver.getCurrentUrl();
+        System.out.println(s);
+
+        String token= GetTokenService.getTokenService(s);
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
-
-//        HttpPost post = new HttpPost("http://yjf3.pccb.com/api/mobile/pwd_control");
-//        List<NameValuePair> list = new ArrayList<NameValuePair>();
-//        list.add(new BasicNameValuePair("app_type","0"));
-//        list.add(new BasicNameValuePair("userid", Des.encryptDES("82044")));
-////        list.add(new BasicNameValuePair("borrow_id",Des.encryptDES("5554")));
-//        list.add(new BasicNameValuePair("sign_key", Md5.md5("0" + "82044" + "pccbGetProjectUserInfo")));
-//        post.setEntity(new UrlEncodedFormEntity(list));
-//        CloseableHttpResponse response = httpclient.execute(post);
-//
-//        System.out.println(response.getStatusLine());
-//        HttpEntity entity2 = response.getEntity();
-//        String entity = EntityUtils.toString(entity2);
-//        System.out.println("response content:" + entity);
         try {
-            HttpPost post = new HttpPost("http://yjf3.pccb.com/api/mobile/pwd_control");
+            HttpPost post = new HttpPost("http://yjf3.pccb.com/api/mobile/invest");
             List<NameValuePair> list = new ArrayList<NameValuePair>();
-            list.add(new BasicNameValuePair("app_type","0"));
+            list.add(new BasicNameValuePair("app_type", "1"));
             list.add(new BasicNameValuePair("userid", Des.encryptDES("82044")));
-            list.add(new BasicNameValuePair("sign_key", Md5.md5("0" + "82044" + "pccbYjfUserPwdControl")));
+            list.add(new BasicNameValuePair("borrow_id", Des.encryptDES("5554")));
+            list.add(new BasicNameValuePair("money", "10000"));
+            list.add(new BasicNameValuePair("coupon", "0"));
+            list.add(new BasicNameValuePair("piggy", "0"));
+            list.add(new BasicNameValuePair("pay_token", Des.encryptDES(token)));
+            list.add(new BasicNameValuePair("sign_key", Md5.md5("1" + "82044" + "5554" + token + "pccbYjfUserInvest")));
+
             post.setEntity(new UrlEncodedFormEntity(list));
             CloseableHttpResponse response = httpclient.execute(post);
 
@@ -53,55 +69,21 @@ public class InvestTest {
                 System.out.println(response.getStatusLine());
                 HttpEntity entity2 = response.getEntity();
                 String entity = EntityUtils.toString(entity2);
-                System.out.println("response content:" + entity);
+                if (entity.contains("")) {
+                    System.out.println("Success!");
+                    System.out.println("response content:" + entity);
+                } else {
+                    System.out.println("Failure!");
+                    System.out.println("response content:" + entity);
+                    AssertJUnit.fail(entity);
+                }
             } finally {
                 response.close();
             }
         }
-        finally {
-            httpclient.close();
+            finally{
+                httpclient.close();
 
-        }
-
-
-
-//        try {
-//            HttpPost post = new HttpPost("http://yjf3.pccb.com/api/mobile/invest");
-//            List<NameValuePair> list = new ArrayList<NameValuePair>();
-//            list.add(new BasicNameValuePair("app_type","0"));
-//            list.add(new BasicNameValuePair("userid", Des.encryptDES("82044")));
-//            list.add(new BasicNameValuePair("borrow_id",Des.encryptDES("5554")));
-//            list.add(new BasicNameValuePair("money","10000"));
-//            list.add(new BasicNameValuePair("coupon","0"));
-//            list.add(new BasicNameValuePair("piggy","0"));
-//            list.add(new BasicNameValuePair("pay_token","TK1512091616n5k8c49442da46e4a7bc506c1f45cc0bbff742bfdbc318186e57"));
-//            list.add(new BasicNameValuePair("sign_key"," "));
-//
-//            post.setEntity(new UrlEncodedFormEntity(list));
-//            CloseableHttpResponse response = httpclient.execute(post);
-//
-//            try {
-//                System.out.println(response.getStatusLine());
-//                HttpEntity entity2 = response.getEntity();
-//                String entity = EntityUtils.toString(entity2);
-//                if(entity.contains("")){
-//                    System.out.println("Success!");
-//                    System.out.println("response content:" + entity);
-//                }
-//                else
-//                {
-//                    System.out.println("Failure!");
-//                    System.out.println("response content:" + entity);
-//                    AssertJUnit.fail(entity);
-//                }
-//            } finally {
-//                response.close();
-//            }
-//        }
-//        finally {
-//            httpclient.close();
-//
-//        }
-
+            }
     }
 }
